6ce8adf2861e7bb11c52116b94054f9aeb684981
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2018-10-24 23:35:04 -0700

    letter showing up, good

--- index.js
+++ index.js
@@ -7,36 +7,77 @@ var inquirer = require("inquirer");
 
 var candidateArr = ["potato","pumpkin","salad","fries","noodle","boba"];
 var guessesLeft = 10;
+var random;
+var candidateWord;
 
-function startGame() {
+generateAns();
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Guess a letter: ',
+            name: 'letter'
+        }
+    ]).then(function(input) {
+
+        checkAnswer(input);
+    });
+
+function generateAns() {
+    console.log("\n--------------------\n");
+
+    random = Math.floor(Math.random() * 6);
+    console.log("The random number is: " + random);
+    console.log("The word is: " + candidateArr[random]);
+    candidateWord = new Word(candidateArr[random]);
+    // candidateWord = new Word(candidateArr[0]);
+    
+    console.log(candidateWord.stringifyWord());
+};
+
+function checkAnswer(letter) {
     console.log("Start the game!");
+    guessesLeft--;
+
+    console.log(candidateWord.wordArr.length);
+    for(var i = 0; i < candidateWord.wordArr.length; i++) {
+        if(letter.letter === candidateWord.wordArr[i].letter) {
+            candidateWord.wordArr[i].checkLetter(letter.letter);
+            console.log("HElloooo?", candidateWord.wordArr[i].reveal());
+        }
+        // if(candidateWord.wordArr[])
+    };
+
+    console.log(candidateWord.stringifyWord());
+
+    // loop inquirer if chances haven't been used up yet
     if(guessesLeft !== 0) {
         
-        var random = Math.floor(Math.random() * 6);
-        console.log("The random number is: " + random);
-        console.log("The word is: " + candidateArr[random]);
-        var candidateWord = new Word(candidateArr[random]);
+        console.log("\nThis is word ", letter.letter);
+        // if(letter !== candidateWord.wordArr) {
 
-        console.log(candidateWord.stringifyWord());
-    
-        inquirer
-            .prompt([
-                {
-                    type: 'input',
-                    message: 'Guess a letter: ',
-                    name: 'letter'
-                }
-            ])
-
-        // need a .then function, pass in the usrinput, and print out the word again
-
-        guessesLeft--;
+        // }
         console.log("Guesses left: " + guessesLeft + "!");
+        tryAgain();
     }
     else {
         guessesLeft = 10;
-        startGame();
     }
 };
 
-startGame();
+function tryAgain() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Guess a letter: ',
+            name: 'letter'
+        }
+    ]).then(function(input) {
+        console.log(candidateWord.stringifyWord());
+        checkAnswer(input);
+    });
+};
+
+// startGame();
